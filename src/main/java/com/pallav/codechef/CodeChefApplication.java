4d38095464423f0d2a20e51def2e983838eef848
Pallav Tyagi <ptyagi6@sapient.com>
2018-04-30 07:02:41 +0530

    intial commit

---
+++ src/main/java/com/pallav/codechef/CodeChefApplication.java
@@ -0,0 +1,12 @@
+package com.pallav.codechef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeChefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeChefApplication.class, args);
+	}
+}
